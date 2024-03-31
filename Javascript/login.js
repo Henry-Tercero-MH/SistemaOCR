0ee7b21e7b04a57ff4592f2e74008d4f42f35cee
Henry-Tercero-MH <tercerohernandezhenrymisael@gmail.com>
2024-03-31 09:40:05 -0600

    Agregando la opcion de mostrar y ocultar contraseña en el login

--- Javascript/login.js
+++ Javascript/login.js
@@ -1,11 +1,15 @@
 function togglePassword() {
   const passwordInput = document.getElementById("password");
+  const icon = document.querySelector(".bx");
+
   // Cambiar el tipo de entrada del campo de contraseña de "password" a "text" y viceversa
   if (passwordInput.type === "password") {
     passwordInput.type = "text";
+    icon.classList.remove("bx-show-alt");
+    icon.src = "img/Hide.ico"; // Cambiar la ruta de la imagen a "img/Hide.ico"
   } else {
     passwordInput.type = "password";
-    eyeOpenIcon.style.display = "inline-block";
-    eyeSlashIcon.style.display = "none";
+    icon.src.remove = "img/Hide.ico"; // Cambiar la ruta de la imagen a "img/Hide.ico"
+    icon.classList.add("bx-hide-alt");
   }
 }
